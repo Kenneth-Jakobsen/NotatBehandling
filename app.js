b37abf5f55b30c5c677d935462ddec056b107b49
ImPhame <jakobsenkenneth@hotmail.no>
2025-01-27 11:17:49 +0100

    x

--- app.js
+++ app.js
@@ -6,9 +6,44 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-rl.question('Enter the name of the file to create: ', (fileName) => {
-    rl.question('Enter the content for the file: ', (content) => {
-        fs.writeFileSync(fileName, content);
-        rl.close();
+function create(){
+    rl.question('Enter the name of the file to create: ', (fileName) => {
+        rl.question('Enter the content for the file: ', (content) => {
+            fs.writeFileSync(fileName, content);
+            rl.close();
+        });
     });
-});
+};
+
+function menu(){
+    console.log('1. Create a file');
+    console.log('2. Read a file');
+    console.log('3. Update a file');
+    console.log('4. Delete a file');
+    console.log('5. Exit');
+    rl.question('Enter your choice: ', (choice) => {
+        switch(choice){
+            case '1':
+                create();
+                break;
+            case '2':
+                read();
+                break;
+            case '3':
+                update();
+                break;
+            case '4':
+                deleteFile();
+                break;
+            case '5':
+                rl.close();
+                break;
+            default:
+                console.log('Invalid choice');
+                menu();
+                break;
+        }
+    });
+}
+
+menu();
